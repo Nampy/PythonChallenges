7f0e44e2ba29d624d36d0c020ad3affeca41d57d
Nampy <at_ben@encs.concordia.ca>
2018-07-29 23:59:56 -0400

    challenge 5 started

--- challenge4.py
+++ challenge4.py
@@ -3,4 +3,4 @@ import re
 
 if __name__ == '__main__':
     file = (urllib.request.urlopen("http://www.pythonchallenge.com/pc/def/equality.html").read().decode())
-    print("".join(re.findall("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+", file)))
+    print("".join(re.findall("[^A-Z][A-Z]{3}([a-z])[A-Z]{3}[^A-Z]", file)))
---
+++ challenge5.py
@@ -0,0 +1,6 @@
+import urllib.request
+
+if __name__ == '__main__':
+    print("beginning of ma life: ")
+    path = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
+    post_num = ""
